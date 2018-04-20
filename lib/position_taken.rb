e31c0aad333bec35c1e00a9a1cb2865dcf5354c5
Wasif Khan <waasif.khaan@gmail.com>
2018-04-20 23:48:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,11 @@
 
 
 def position_taken?(board ,index)
-if board[index] == " " || board[index] == "" || board[index] == nil
-false
-elsif board[index] == "X" || board[index] == "O"
-true
-else
-end
+  if board[index] == " " || board[index] == "" || board[index] == nil
+      false
+
+#elsif board[index] == "X" || board[index] == "O"
+  else
+      true
+  end
 end
