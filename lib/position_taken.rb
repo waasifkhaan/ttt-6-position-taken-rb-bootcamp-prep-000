31e601c6213b3b60b5f8188590175dab23988776
Wasif Khan <waasif.khaan@gmail.com>
2018-04-20 23:20:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board ,index)
-if board[index]==[" "]||[""]||[nil]
+if board[index]==" "||""||nil
 false
 elsif board[index]==["X"]||["O"]
 true
