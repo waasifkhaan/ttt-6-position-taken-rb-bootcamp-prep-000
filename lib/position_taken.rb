f712ae650a4d361536c0b7f7bff610c1d470b07f
Wasif Khan <waasif.khaan@gmail.com>
2018-04-20 22:51:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,8 @@
 def position_taken?(board ,index)
   if board[index]= [" "]||[""]||[nil]
     false
-  elsif board[index]=[" X "]||[" O "]
+  else board[index]=[" X "]||[" O "]
+    true
     
   end
 
